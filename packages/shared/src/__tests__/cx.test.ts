ff4b6811ce196f0e5a1dcaaa13320e1e3294a12e
Haroen Viaene <hello@haroen.me>
2022-10-13 17:25:23 +0200

    test(shared): test the type output of cx

--- packages/shared/src/__tests__/cx.test.ts
+++ packages/shared/src/__tests__/cx.test.ts
@@ -30,3 +30,34 @@ describe('cx', () => {
     ).toBe('class1 class3');
   });
 });
+
+// Type tests
+// helpers extracted from https://github.com/TypeStrong/ts-expect
+const expectType = <TType>(_: TType): void => undefined;
+type TypeEqual<TTarget, TValue> = (<TActual>() => TActual extends TTarget
+  ? 1
+  : 2) extends <TActual>() => TActual extends TValue ? 1 : 2
+  ? true
+  : false;
+
+{
+  const actual = cx('class1', 'class2');
+  expectType<TypeEqual<'class1 class2', typeof actual>>(true);
+}
+
+{
+  const actual = cx('class1', false);
+  expectType<TypeEqual<'class1', typeof actual>>(true);
+}
+
+{
+  const actual = cx('class1', ['class2']);
+  // nested arrays lose typing because they aren't `const`
+  expectType<TypeEqual<string, typeof actual>>(true);
+}
+
+{
+  const actual = cx(false, undefined, null, 0, '');
+  // nested arrays lose typing because they aren't `const`
+  expectType<TypeEqual<undefined, typeof actual>>(true);
+}

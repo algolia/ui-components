2ec07513922d73b1387e4b7024392d49c7c517bb
Haroen Viaene <hello@haroen.me>
2022-10-17 11:25:30 +0200

    fix(cx): return a consistent string type (#21)
    * fix(cx): return a consistent string type
    
    This has a downside that if you call cx with values that are all falsy, an empty class will be written, but upside that you can rely on string manipulation later if needed.
    
    This changes the snapshot of highlight, but not the real life usage, as that's called with a string that definitely exists.
    
    This isn't a breaking change in my opinion, as an empty className and no className act identical, and this solution is much simpler than #20 or changing all code related to classNames in InstantSearch to have every key optional (even if they actually aren't going to be undefined)
    
    For reference, the `classnames` package is also typed to always return a string.
    
    * Update packages/shared/src/__tests__/cx.test.ts
    
    Co-authored-by: Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
    
    Co-authored-by: Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>

--- packages/shared/src/cx.ts
+++ packages/shared/src/cx.ts
@@ -1,15 +1,13 @@
 type ClassValue = string | undefined | boolean | null | number;
 
 export function cx(...cssClasses: Array<ClassValue | ClassValue[]>) {
-  return (
-    cssClasses
-      .reduce<ClassValue[]>((acc, className) => {
-        if (Array.isArray(className)) {
-          return acc.concat(className);
-        }
-        return acc.concat([className]);
-      }, [])
-      .filter(Boolean)
-      .join(' ') || undefined
-  );
+  return cssClasses
+    .reduce<ClassValue[]>((acc, className) => {
+      if (Array.isArray(className)) {
+        return acc.concat(className);
+      }
+      return acc.concat([className]);
+    }, [])
+    .filter(Boolean)
+    .join(' ');
 }
